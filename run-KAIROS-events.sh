f276751b2566e507b8e456d41a617200bc63f46a
Yuqian Deng <68020340+Michael-Dyq@users.noreply.github.com>
2021-12-04 20:43:59 -0500

    Update run-KAIROS-events.sh
    redirect gurobi_client.lic

--- run-KAIROS-events.sh
+++ run-KAIROS-events.sh
@@ -1,5 +1,6 @@
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
-export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+#export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+export GRB_LICENSE_FILE=/shared/ccgadmin/app/gurobi/gurobi_client.lic
 while [ 1 -eq 1 ]
 do 
 	python backend.py
