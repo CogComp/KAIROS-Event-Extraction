fa614e27b7e2d85bd45d5f42a7c34fa2deb005dc
Anjana Tiha <anjana.tiha@gmail.com>
2022-01-27 14:26:53 -0500

    Update run-KAIROS-events.sh

--- run-KAIROS-events.sh
+++ run-KAIROS-events.sh
@@ -1,7 +1,7 @@
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
 export GRB_LICENSE_FILE=/shared/ccgadmin/app/gurobi/gurobi_client.lic
-while [ 1 -eq 1 ]
+while true
 do 
 	python backend.py
 done
