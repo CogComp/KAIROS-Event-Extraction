6471e93c66bd161127818920791d6fd917f83f78
Anjana Tiha <anjana.tiha@gmail.com>
2022-03-16 12:06:23 -0400

    Update run-KAIROS-events.sh

--- run-KAIROS-events.sh
+++ run-KAIROS-events.sh
@@ -1,8 +1,11 @@
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi.lic
 #export GRB_LICENSE_FILE=/shared/ccgadmin/demos/KAIROS2020-demo/events/gurobi_client_server.lic
+export CUDA_VISIBLE_DEVICES=0,1
+#export CUDA_VISIBLE_DEVICES=0
 export GRB_LICENSE_FILE=/shared/ccgadmin/app/gurobi/gurobi_client.lic
 while true
 do 
-	python backend.py
+	# python backend.py
+	python backend_new.py
 done
 
